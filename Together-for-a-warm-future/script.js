dbd2e43ee5316ccc3dd375981ea710c23613e58a
Daniel <dani94@mail.bg>
2026-09-12 17:30:37 +0300

    Add JavaScript to showcase

---
+++ Together-for-a-warm-future/script.js
@@ -0,0 +1,14 @@
+function sayThanks() {
+  event.preventDefault();
+  alert("Вие заплатихте успешно, благодаря за вашата подкрепа и разбиране към нуждаещите се!");
+ 
+  window.location.href = "index.html"; 
+}
+
+function remindUsers() {
+  event.preventDefault();
+  alert("Успешно получихме вашата заявка, при първа възможност ще се свържем с Вас!");
+ 
+  window.location.href = "About us.html"; 
+}
+
